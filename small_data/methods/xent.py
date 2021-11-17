4c97cb6c83c48ddda04466e2f2462f677726fbc6
lorenzobrigato <45622297+lorenzobrigato@users.noreply.github.com>
2021-11-17 17:41:15 +0100

    Removed RandAugment

--- small_data/methods/xent.py
+++ small_data/methods/xent.py
@@ -1,24 +1,13 @@
 from torch import nn
 from typing import Callable
 
-from .common import BasicAugmentation, RandAugmentAugmentation
+from .common import BasicAugmentation
 
 
 class CrossEntropyClassifier(BasicAugmentation):
     """ Standard cross-entropy classification as baseline.
 
     See `BasicAugmentation` for a documentation of the available hyper-parameters.
-    """
-
-    def get_loss_function(self) -> Callable:
-
-        return nn.CrossEntropyLoss(reduction='mean')
-    
-    
-class CrossEntropyRandAugmentClassifier(RandAugmentAugmentation):
-    """ Standard cross-entropy classification with additional RandAugment data augmentation.
-
-    See `RandAugmentAugmentation` for a documentation of the available hyper-parameters.
     """
 
     def get_loss_function(self) -> Callable:
